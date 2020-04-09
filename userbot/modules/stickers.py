0210d68638779cdc361a06ded40ec270564bf0fc
ashwinstr <62101864+ashwinstr@users.noreply.github.com>
2020-04-09 09:11:42 +0530

    Update stickers.py

--- userbot/modules/stickers.py
+++ userbot/modules/stickers.py
@@ -313,11 +313,11 @@ CMD_HELP.update({
     "stickers":
     ".kangme\
 \nUsage: Reply .kang to a sticker or an image to kang it to your userbot pack.\
-\n\n.kang [emoji('s)]\
+\n\n.kangme [emoji('s)]\
 \nUsage: Works just like .kang but uses the emoji('s) you picked.\
-\n\n.kang [number]\
+\n\n.kangme [number]\
 \nUsage: Kang's the sticker/image to the specified pack but uses 🤔 as emoji.\
-\n\n.kang [emoji('s)] [number]\
+\n\n.kangme [emoji('s)] [number]\
 \nUsage: Kang's the sticker/image to the specified pack and uses the emoji('s) you picked.\
 \n\n.stkrinfo\
 \nUsage: Gets info about the sticker pack."
