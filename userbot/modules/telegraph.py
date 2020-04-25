9d2a36e2a8853a6eeaa3ca0f9641f2432e5fab22
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2020-04-25 14:52:17 +0530

    Update telegraph.py

--- userbot/modules/telegraph.py
+++ userbot/modules/telegraph.py
@@ -1,10 +1,3 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
-#
-# Licensed under the Raphielscape Public License, Version 1.d (the "License");
-# you may not use this file except in compliance with the License.
-#
-# Port From UniBorg to UserBot by @afdulfauzan
-
 from telethon import events
 import os
 from PIL import Image
@@ -13,72 +6,82 @@ from telegraph import Telegraph, upload_file, exceptions
 from userbot import (TELEGRAPH_SHORT_NAME, TEMP_DOWNLOAD_DIRECTORY, BOTLOG_CHATID, CMD_HELP, bot)
 from userbot.events import register
 
+PRIVATE_GROUP_BOT_API_ID = BOTLOG_CHATID
+
 telegraph = Telegraph()
 r = telegraph.create_account(short_name=TELEGRAPH_SHORT_NAME)
 auth_url = r["auth_url"]
 
 
 @register(outgoing=True, pattern="^.tg (media|text)$")
-async def telegraphs(graph):
-    """ For .telegraph command, upload media & text to telegraph site. """
-    if not graph.text[0].isalpha() and graph.text[0] not in ("/", "#", "@", "!"):
-        if graph.fwd_from:
-            return
-        if not os.path.isdir(TEMP_DOWNLOAD_DIRECTORY):
-            os.makedirs(TEMP_DOWNLOAD_DIRECTORY)
-        if graph.reply_to_msg_id:
-            start = datetime.now()
-            r_message = await graph.get_reply_message()
-            input_str = graph.pattern_match.group(1)
-            if input_str == "media":
-                downloaded_file_name = await bot.download_media(
+async def telegraph(event):
+    if event.fwd_from:
+        return
+    if PRIVATE_GROUP_BOT_API_ID is None:
+        await event.edit("Please set the required environment variable `PRIVATE_GROUP_BOT_API_ID` for this plugin to work")
+        return
+    if not os.path.isdir(TEMP_DOWNLOAD_DIRECTORY):
+        os.makedirs(TEMP_DOWNLOAD_DIRECTORY)
+    await borg.send_message(
+        PRIVATE_GROUP_BOT_API_ID,
+        "Created New Telegraph account {} for the current session. \n**Do not give this url to anyone, even if they say they are from Telegram!**".format(auth_url)
+    )
+    optional_title = event.pattern_match.group(2)
+    if event.reply_to_msg_id:
+        start = datetime.now()
+        r_message = await event.get_reply_message()
+        input_str = event.pattern_match.group(1)
+        if input_str == "media":
+            downloaded_file_name = await borg.download_media(
+                r_message,
+                TEMP_DOWNLOAD_DIRECTORY
+            )
+            end = datetime.now()
+            ms = (end - start).seconds
+            await event.edit("Downloaded to {} in {} seconds.".format(downloaded_file_name, ms))
+            if downloaded_file_name.endswith((".webp")):
+                resize_image(downloaded_file_name)
+            try:
+                start = datetime.now()
+                media_urls = upload_file(downloaded_file_name)
+            except exceptions.TelegraphException as exc:
+                await event.edit("ERROR: " + str(exc))
+                os.remove(downloaded_file_name)
+            else:
+                end = datetime.now()
+                ms_two = (end - start).seconds
+                os.remove(downloaded_file_name)
+                await event.edit("Uploaded to https://telegra.ph{} in {} seconds.".format(media_urls[0], (ms + ms_two)), link_preview=True)
+        elif input_str == "text":
+            user_object = await borg.get_entity(r_message.from_id)
+            title_of_page = user_object.first_name # + " " + user_object.last_name
+            # apparently, all Users do not have last_name field
+            if optional_title:
+                title_of_page = optional_title
+            page_content = r_message.message
+            if r_message.media:
+                if page_content != "":
+                    title_of_page = page_content
+                downloaded_file_name = await borg.download_media(
                     r_message,
                     TEMP_DOWNLOAD_DIRECTORY
                 )
-                end = datetime.now()
-                ms = (end - start).seconds
-                await graph.edit("Downloaded to {} in {} seconds.".format(downloaded_file_name, ms))
-                if downloaded_file_name.endswith((".webp")):
-                    resize_image(downloaded_file_name)
-                try:
-                    start = datetime.now()
-                    media_urls = upload_file(downloaded_file_name)
-                except exceptions.TelegraphException as exc:
-                    await graph.edit("ERROR: " + str(exc))
-                    os.remove(downloaded_file_name)
-                else:
-                    end = datetime.now()
-                    ms_two = (end - start).seconds
-                    os.remove(downloaded_file_name)
-                    await graph.edit("Uploaded to https://telegra.ph{} in {} seconds.".format(media_urls[0], (ms + ms_two)), link_preview=True)
-            elif input_str == "text":
-                user_object = await bot.get_entity(r_message.from_id)
-                title_of_page = user_object.first_name # + " " + user_object.last_name
-                # apparently, all Users do not have last_name field
-                page_content = r_message.message
-                if r_message.media:
-                    if page_content != "":
-                        title_of_page = page_content
-                    downloaded_file_name = await bot.download_media(
-                        r_message,
-                        TEMP_DOWNLOAD_DIRECTORY
-                    )
-                    m_list = None
-                    with open(downloaded_file_name, "rb") as fd:
-                        m_list = fd.readlines()
-                    for m in m_list:
-                        page_content += m.decode("UTF-8") + "\n"
-                    os.remove(downloaded_file_name)
-                page_content = page_content.replace("\n", "<br>")
-                response = telegraph.create_page(
-                    title_of_page,
-                    html_content=page_content
-                )
-                end = datetime.now()
-                ms = (end - start).seconds
-                await graph.edit("Pasted to https://telegra.ph/{} in {} seconds.".format(response["path"], ms), link_preview=True)
-        else:
-            await graph.edit("Reply to a message to get a permanent telegra.ph link. (Inspired by @ControllerBot)")
+                m_list = None
+                with open(downloaded_file_name, "rb") as fd:
+                    m_list = fd.readlines()
+                for m in m_list:
+                    page_content += m.decode("UTF-8") + "\n"
+                os.remove(downloaded_file_name)
+            page_content = page_content.replace("\n", "<br>")
+            response = telegraph.create_page(
+                title_of_page,
+                html_content=page_content
+            )
+            end = datetime.now()
+            ms = (end - start).seconds
+            await event.edit("Pasted to https://telegra.ph/{} in {} seconds.".format(response["path"], ms), link_preview=True)
+    else:
+        await event.edit("Reply to a message to get a permanent telegra.ph link. (Inspired by @ControllerBot)")
 
 
 def resize_image(image):
