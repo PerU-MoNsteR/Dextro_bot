d2202c009e1bd02be2d6b1a8416d6ccb546214e2
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2020-04-30 12:50:19 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -3,6 +3,7 @@
 # Licensed under the Raphielscape Public License, Version 1.c (the "License");
 # you may not use this file except in compliance with the License.
 #
+# thanks to penn5 for bug fixing
 """ Userbot initialization. """
 
 import os
