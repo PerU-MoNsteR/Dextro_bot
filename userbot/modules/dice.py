ce8af67417f0e70db69ac181f9829efdeb17c53c
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2020-05-08 01:42:03 +0530

    Update dice.py

--- userbot/modules/dice.py
+++ userbot/modules/dice.py
@@ -14,13 +14,13 @@ async def _(event):
         return
     input_str = event.pattern_match.group(1)
     await event.delete()
-    r = await event.reply(file=InputMediaDice())
+    r = await event.reply(file=InputMediaDice(''))
     if input_str:
         try:
             required_number = int(input_str)
             while not r.media.value == required_number:
                 await r.delete()
-                r = await event.reply(file=InputMediaDice())
+                r = await event.reply(file=InputMediaDice(''))
         except:
             pass
 
