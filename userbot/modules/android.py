95151cd6e5bd8b877a518b57e344fa8f5b7642c5
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2020-05-13 18:20:24 +0530

    Update android.py

--- userbot/modules/android.py
+++ userbot/modules/android.py
@@ -93,7 +93,8 @@ async def codename_info(request):
     ]
     if results:
         reply = f"**Search results for {brand} {device}**:\n\n"
-        if len(results) > 8:
+        if len(results) > 
+8:
             results = results[:8]
         for item in results:
             reply += f"**Device**: {item['device']}\n" \
@@ -102,7 +103,6 @@ async def codename_info(request):
     else:
         reply = f"`Couldn't find {device} codename!`\n"
     await request.edit(reply)
-
 
 @register(outgoing=True, pattern=r"^.specs(?: |)([\S]*)(?: |)([\s\S]*)")
 async def devices_specifications(request):
