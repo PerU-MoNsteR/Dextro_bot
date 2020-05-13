731f7de3e908dbb1e795a67a8811eecfb7c9d8ca
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2020-05-13 17:16:15 +0530

    Update screencapture.py

--- userbot/modules/screencapture.py
+++ userbot/modules/screencapture.py
@@ -8,52 +8,63 @@
 
 import io
 import traceback
-import requests
-from telethon import events
 from re import match
 from selenium import webdriver
 from asyncio import sleep
 from selenium.webdriver.chrome.options import Options
 from userbot.events import register
-from userbot import SCREEN_SHOT_LAYER_ACCESS_KEY, CMD_HELP
+from userbot import GOOGLE_CHROME_BIN, CHROME_DRIVER, CMD_HELP
 
 
 @register(outgoing=True, pattern="^.ss$")
-async def _(event):
-    if event.fwd_from:
-        return
-    if SCREEN_SHOT_LAYER_ACCESS_KEY is None:
-        await event.edit("Need to get an API key from https://screenshotlayer.com/dashboard \nModule stopping!")
-        return
-    await event.edit("Processing ...")
-    sample_url = "https://api.screenshotlayer.com/api/capture?access_key={}&url={}&fullpage={}&viewport={}&format={}&force={}"
-    input_str = event.pattern_match.group(1)
-    response_api = requests.get(sample_url.format(
-        SCREEN_SHOT_LAYER_ACCESS_KEY,
-        input_str,
-        "1",
-        "2560x1440",
-        "PNG",
-        "1"
-    ))
-    # https://stackoverflow.com/a/23718458/4723940
-    contentType = response_api.headers['content-type']
-    if "image" in contentType:
-        with io.BytesIO(response_api.content) as screenshot_image:
-            screenshot_image.name = "screencapture.png"
-            try:
-                await borg.send_file(
-                    event.chat_id,
-                    screenshot_image,
-                    caption=input_str,
-                    force_document=True,
-                    reply_to=event.message.reply_to_msg_id
-                )
-                await event.delete()
-            except Exception as e:
-                await event.edit(str(e))
+async def capture(url):
+    """ For .ss command, capture a website's screenshot and send the photo. """
+    await url.edit("`Processing ...`")
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--test-type")
+    chrome_options.binary_location = GOOGLE_CHROME_BIN
+    chrome_options.add_argument('--ignore-certificate-errors')
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument('--disable-gpu')
+    driver = webdriver.Chrome(executable_path=CHROME_DRIVER,
+                              options=chrome_options)
+    input_str = url.pattern_match.group(1)
+    link_match = match(r'\bhttps?://.*\.\S+', input_str)
+    if link_match:
+        link = link_match.group()
     else:
-        await event.edit(response_api.text)
+        await url.edit("`I need a valid link to take screenshots from.`")
+        return
+    driver.get(link)
+    height = driver.execute_script(
+        "return Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);"
+    )
+    width = driver.execute_script(
+        "return Math.max(document.body.scrollWidth, document.body.offsetWidth, document.documentElement.clientWidth, document.documentElement.scrollWidth, document.documentElement.offsetWidth);"
+    )
+    driver.set_window_size(width + 125, height + 125)
+    wait_for = height / 1000
+    await url.edit(f"`Generating screenshot of the page...`\
+    \n`Height of page = {height}px`\
+    \n`Width of page = {width}px`\
+    \n`Waiting ({int(wait_for)}s) for the page to load.`")
+    await sleep(int(wait_for))
+    im_png = driver.get_screenshot_as_png()
+    # saves screenshot of entire page
+    driver.close()
+    message_id = url.message.id
+    if url.reply_to_msg_id:
+        message_id = url.reply_to_msg_id
+    with io.BytesIO(im_png) as out_file:
+        out_file.name = "screencapture.png"
+        await url.edit("`Uploading screenshot as file..`")
+        await url.client.send_file(url.chat_id,
+                                   out_file,
+                                   caption=input_str,
+                                   force_document=True,
+                                   reply_to=message_id)
 
 
 CMD_HELP.update({
@@ -62,3 +73,4 @@ CMD_HELP.update({
     \nUsage: Takes a screenshot of a website and sends the screenshot.\
     \nExample of a valid URL : `https://www.google.com`"
 })
+
