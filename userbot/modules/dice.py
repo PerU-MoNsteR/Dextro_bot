441bc1ec8caed3cd8d310908ff16025083dc2d55
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2020-05-14 07:27:01 +0530

    Update dice.py
    dart added now

--- userbot/modules/dice.py
+++ userbot/modules/dice.py
@@ -2,7 +2,6 @@
 #bug fixed by @d3athwarrior
 
 from telethon.tl.types import InputMediaDice
-#from uniborg.util import admin_cmd
 from userbot.events import register 
 from userbot import BOTLOG, BOTLOG_CHATID, CMD_HELP, bot, ALIVE_NAME
 
@@ -23,10 +22,32 @@ async def _(event):
                 r = await event.reply(file=InputMediaDice(''))
         except:
             pass
+        
+@register(outgoing=True, pattern="^.dart(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    input_str = event.pattern_match.group(1)
+    await event.delete()
+    r = await event.reply(file=InputMediaDice('🎯'))
+    if input_str:
+        try:
+            required_number = int(input_str)
+            while not r.media.value == required_number:
+                await r.delete()
+                r = await event.reply(file=InputMediaDice('🎯'))
+        except:
+            pass        
 
         
 CMD_HELP.update({
     "dice":
     ".dice or .dice 1 to 6 any value\
+\nUsage: hahaha just a magic."
+})    
+
+CMD_HELP.update({
+    "dart":
+    ".dart or .dart 1 to 6 any value\
 \nUsage: hahaha just a magic."
 })    
