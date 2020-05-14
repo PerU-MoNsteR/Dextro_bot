c65810893b267bd36184271d466d11d16efdbecc
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2020-05-14 23:47:35 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -158,7 +158,6 @@ G_DRIVE_CLIENT_ID = os.environ.get("G_DRIVE_CLIENT_ID", None)
 G_DRIVE_CLIENT_SECRET = os.environ.get("G_DRIVE_CLIENT_SECRET", None)
 G_DRIVE_AUTH_TOKEN_DATA = os.environ.get("G_DRIVE_AUTH_TOKEN_DATA", None)
 GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID", None)
-GDRIVE_FOLDER = os.environ.get("GDRIVE_FOLDER", None)
 TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TMP_DOWNLOAD_DIRECTORY",
                                          "./downloads")
 
