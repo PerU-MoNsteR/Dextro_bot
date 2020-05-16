dd32d24029b6336a0a56251d59b1b5b7f2caf889
Devp73 <52715880+Devp73@users.noreply.github.com>
2020-05-16 10:31:36 +0530

    Lastname error fix

--- userbot/modules/afk.py
+++ userbot/modules/afk.py
@@ -84,8 +84,11 @@ async def set_afk(afk_e):
         await afk_e.edit("**Going AFK!**")
     else:
         await afk_e.edit("**Going AFK!**")
-    await afk_e.client(
-        UpdateProfileRequest(first_name=user.first_name, last_name=user.last_name + " [ OFFLINE ]"))
+    if user.last_name:
+        await afk_e.client(UpdateProfileRequest(first_name=user.first_name, last_name=user.last_name + " [ OFFLINE ]"))
+    else:
+        await afk_e.client(UpdateProfileRequest(first_name=user.first_name, last_name=" [ OFFLINE ]"))
+
     if BOTLOG:
         await afk_e.client.send_message(BOTLOG_CHATID, "#AFK\nYou went AFK!")
     ISAFK = True
@@ -106,7 +109,10 @@ async def type_afk_is_not_true(notafk):
     global afk_end
     user = await bot.get_me()
     last = user.last_name
-    last1 = last[:-12]
+    if last and last.endswith(" [ OFFLINE ]"):
+        last1 = last[:-12]
+    else:
+        last1 = ""
     back_alive = datetime.now()
     afk_end = back_alive.replace(microsecond=0)
     if ISAFK:
