bb6a5e0bc54aa77967533c802368a91b599ed13a
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2020-06-03 17:43:05 +0530

    Update admin.py

--- userbot/modules/admin.py
+++ userbot/modules/admin.py
@@ -815,6 +815,195 @@ async def get_user_from_id(user, event):
 
     return user_obj
 
+#imported from uniborg by @heyworld
+@register(outgoing=True, pattern="^.warn(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    chat = await event.get_chat()
+    admin = chat.admin_rights
+    creator = chat.creator
+    warn_reason = event.pattern_match.group(1)
+    reply_message = await event.get_reply_message()
+    
+    if not admin and not creator:
+        await event.edit("`Bruh I Am Not Admin Here`")
+        return
+    
+    if await is_admin(event.chat_id, reply_message.from_id):
+        return await event.edit("`User is an admin`")
+
+    limit, soft_warn = sql.get_warn_setting(event.chat_id)
+    num_warns, reasons = sql.warn_user(reply_message.from_id, event.chat_id, warn_reason)
+    if num_warns >= limit:
+        await event.client.edit_permissions(chat, reply_message.from_id, until_date=None, view_messages=False)
+        if soft_warn:
+            reply = "{} warnings, <u><a href='tg://user?id={}'>user</a></u> has been kicked!".format(limit, reply_message.from_id)
+            await event.client.kick_participant(event.chat_id, reply_message.from_id)
+        else:
+            await event.client.edit_permissions(chat, reply_message.from_id, until_date=None, view_messages=False)
+            reply = "{} warnings, <u><a href='tg://user?id={}'>user</a></u> has been banned!".format(limit, reply_message.from_id)
+    else:
+        reply = "<u><a href='tg://user?id={}'>user</a></u> has {}/{} warnings... watch out!".format(reply_message.from_id, num_warns, limit)
+        if warn_reason:
+            reply += "\nReason for last warn:\n{}".format(html.escape(warn_reason))
+    #
+    await event.edit(reply, parse_mode="html")
+
+@register(outgoing=True, pattern="^.getwarns(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    reply_message = await event.get_reply_message()
+    result = sql.get_warns(reply_message.from_id, event.chat_id)
+    if result and result[0] != 0:
+        num_warns, reasons = result
+        limit, soft_warn = sql.get_warn_setting(event.chat_id)
+        if reasons:
+            text = "This user has {}/{} warnings, for the following reasons:".format(num_warns, limit)
+            text += "\r\n"
+            text += reasons
+            await event.edit(text)
+        else:
+            await event.edit("This user has {} / {} warning, but no reasons for any of them.".format(num_warns, limit))
+    else:
+        await event.edit("This user hasn't got any warnings!")
+
+@register(outgoing=True, pattern="^.strongwarn(?: |$)(.*)")
+async def set_warn_strength(event):
+    chat = await event.get_chat()
+    admin = chat.admin_rights
+    creator = chat.creator
+    args = event.pattern_match.group(1)
+
+    if not admin and not creator:
+        await event.edit("`Bruh I Am Not Admin Here`")
+        return
+
+    if args:
+        if args in ("on", "yes"):
+            sql.set_warn_strength(event.chat_id, False)
+            await event.edit("Warn Strength Set To Ban User.")
+            return
+
+        elif args in ("off", "no"):
+            sql.set_warn_strength(event.chat_id, True)
+            await event.edit("Warn Strength Set To Kick User.")
+            return
+       
+        else:
+            await event.edit("`Please send Correct Arg!`")
+    else:
+        limit, soft_warn = sql.get_warn_setting(event.chat_id)
+        if soft_warn:
+            await event.edit("I Am **kicking** User's For Now.")
+        else:
+            await event.edit("I Am **Baning** User's For Now.")
+    return ""
+
+@register(outgoing=True, pattern="^.setwarn(?: |$)(.*)")
+async def set_warn_limit(event):
+    chat = await event.get_chat()
+    admin = chat.admin_rights
+    creator = chat.creator
+    input_str = event.pattern_match.group(1)
+
+    if not admin and not creator:
+        await event.edit("`Bruh I Am Not Admin Here`")
+        return
+    
+    if input_str:
+        if int(input_str) < 3:
+            await event.edit("`The minimum warn limit is 3!`")
+        else:
+            sql.set_warn_limit(event.chat_id, int(input_str))
+            await event.edit("`Updated the warn limit to` {}".format(input_str))
+            return
+        
+    else:
+        limit, soft_warn = sql.get_warn_setting(event.chat_id)
+        await event.edit("`The current warn limit is {}`".format(limit))
+    return ""        
+
+
+@register(outgoing=True, pattern="^.resetwarns(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    reply_message = await event.get_reply_message()
+    sql.reset_warns(reply_message.from_id, event.chat_id)
+    await event.edit("Warnings have been reset!") 
+
+@register(incoming=True, disable_edited=True, disable_errors=True)
+async def on_new_message(event):
+    if await is_admin(event.client, even.chat_id, event.from_id):
+        return
+    if bot.me.id == event.from_id:
+        return
+    name = event.raw_text
+    snips = sql.get_chat_blacklist(event.chat_id)
+    for snip in snips:
+        pattern = r"( |^|[^\w])" + re.escape(snip) + r"( |$|[^\w])"
+        if re.search(pattern, name, flags=re.IGNORECASE):
+            try:
+                await event.delete()
+            except Exception as e:
+                await event.reply("I do not have DELETE permission in this chat")
+                await sleep(1)
+                await reply.delete()
+                sql.rm_from_blacklist(event.chat_id, snip.lower())
+            break
+        pass
+
+
+
+
+@register(outgoing=True, pattern="^.addbl(?: |$)(.*)")
+async def on_add_black_list(addbl):
+    text = addbl.pattern_match.group(1)
+    to_blacklist = list(set(trigger.strip() for trigger in text.split("\n") if trigger.strip()))
+    for trigger in to_blacklist:
+        sql.add_to_blacklist(addbl.chat_id, trigger.lower())
+    await addbl.edit("`Added` **{}** `to the blacklist in the current chat`".format(text))
+
+
+@register(outgoing=True, pattern="^.listbl(?: |$)(.*)")
+async def on_view_blacklist(listbl):
+    all_blacklisted = sql.get_chat_blacklist(listbl.chat_id)
+    OUT_STR = "Blacklists in the Current Chat:\n"
+    if len(all_blacklisted) > 0:
+        for trigger in all_blacklisted:
+            OUT_STR += f"`{trigger}`\n"
+    else:
+        OUT_STR = "`There are no blacklist in current chat.`"
+    if len(OUT_STR) > 4096:
+        with io.BytesIO(str.encode(OUT_STR)) as out_file:
+            out_file.name = "blacklist.text"
+            await listbl.client.send_file(
+                listbl.chat_id,
+                out_file,
+                force_document=True,
+                allow_cache=False,
+                caption="BlackLists in the Current Chat",
+                reply_to=listbl
+            )
+            await listbl.delete()
+    else:
+        await listbl.edit(OUT_STR)
+
+
+@register(outgoing=True, pattern="^.rmbl(?: |$)(.*)")
+async def on_delete_blacklist(rmbl):
+    text = rmbl.pattern_match.group(1)
+    to_unblacklist = list(set(trigger.strip() for trigger in text.split("\n") if trigger.strip()))
+    successful = 0
+    for trigger in to_unblacklist:
+        if sql.rm_from_blacklist(rmbl.chat_id, trigger.lower()):
+            successful += 1
+    if not successful:
+        await rmbl.edit("`Blacklist` **{}** `doesn't exist.`".format(text))
+    else:
+        await rmbl.edit("`Blacklist` **{}** `was deleted successfully`".format(text))
   
 @register(outgoing=True, pattern="^.usersdel ?(.*)")
 async def get_usersdel(show):
@@ -975,5 +1164,26 @@ CMD_HELP.update({
 \n\n.users or .users <name of member>\
 \nUsage: Retrieves all (or queried) users in the chat.\
 \n\n.setgppic <reply to image>\
-\nUsage: Changes the group's display picture."
+\nUsage: Changes the group's display picture.\
+\n\n`.warn reason`\
+\nUsage: warns users.\
+\n\n`.resetwarns`\
+\nUsage: Reset user's warns.\
+\n\n`.getwarns`\
+\nUsage: Shows the reason of warning.\
+\n\n`.listbl`\
+\nUsage: Lists all active userbot blacklist in a chat.\
+\n\n`.addbl <keyword>`\
+\nUsage: Saves the message to the 'blacklist keyword.\n\nThe bot will delete to the message whenever 'blacklist keyword' is mentioned.\
+\n\n`.rmbl <keyword>`\
+\nUsage: Stops the specified blacklist.\
+\n\n`.setflood` value.\
+\nUsage:Sets flood limit in the current chat.\
+\n\n`.strongwarn` <yes/on or no/off>.\
+\nUsage:sets warn mode i.e <strong warn:bans user, soft warn: kicks user>.\
+\n\n`.kick` reply or userid.\
+\nUsage: kicks user.\
+\n\n`.setwarn` value.\
+\nUsage:sets warn limit."
+})
 })
