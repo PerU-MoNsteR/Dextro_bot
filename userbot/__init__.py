5b15c9f911263abcabc122bb4d05ec5cd78f62e1
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2020-09-22 21:41:56 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -195,7 +195,6 @@ def is_mongo_alive():
         return False
     return True
 
-
 # Init Redis
 # Redis will be hosted inside the docker container that hosts the bot
 # We need redis for just caching, so we just leave it to non-persistent
