1056112c9b846ed2d635941e26fa1952a2d7cb95
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2020-09-28 20:32:44 +0530

    Update logpms.py
    testing some short of things

--- userbot/modules/logpms.py
+++ userbot/modules/logpms.py
@@ -30,10 +30,10 @@ async def monito_p_m_s(event):
                 e = await event.client.get_entity(int(PM_LOGGR_BOT_API_ID))
                 fwd_message = await event.client.forward_messages(
                     e,
-                    event.message,
-                    silent=True
                     "#Chat\n" + "With " +
                         f"[{chat.first_name}](tg://user?id={chat.id})",
+                    event.message,
+                    silent=True
                 )
             except Exception as e:
                 LOGS.warn(str(e))
